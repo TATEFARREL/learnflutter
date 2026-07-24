fb855b290ed47179542e0a27f5369c2cce0627df
Harold F. B. Tate <41803932+TATEFARREL@users.noreply.github.com>
2026-07-24 20:43:00 +0000

    had fun with inferred and explicit variables

--- syntax.dart
+++ syntax.dart
@@ -1,3 +1,23 @@
 void main(){
-    printf('Hello! Dart\n\t brievity is the soul of wit');
+  // my first comment in Dart
+
+  // let me review the syntax
+
+  // 1. variables in dart
+
+  // type inferencing
+  var name = 'tate';
+  var age = 26;
+
+  // explicit types
+
+  String city = 'Warsaw';
+  int population = 1700000;
+  double temperature = 22.5;
+  bool isSunny = true;
+
+  // i really hate just typing these types, lets try string interpollation
+
+  String about = 'Hello guys, I am ${name} of age ${age}, I have lived in ${city} it is most known to have a population of ${population} and temperature of ${temperature}.';
+  print(about);
 }
